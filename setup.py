962c891c23dff5fc00d0a35a3a05cfb6a50376a1
lee123kkk <lee123kk23@gmail.com>
2026-04-13 11:04:23 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_package_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='lee123kkk',
+    maintainer_email='lee123kkk@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'talker = my_package_py.simple_publisher:main',
+            'listener = my_package_py.simple_subscriber:main',
+            'service_server = my_package_py.simple_service_server:main',
+            'service_client = my_package_py.simple_service_client:main',
+            'custom_talker = my_package_py.custom_publisher:main',
+            'turtle_cmd = my_package_py.turtle_controller:main',
+            'fibonacci_server = my_package_py.fibonacci_action_server:main',
+            'fibonacci_client = my_package_py.fibonacci_action_client:main',
+            'nav_server = my_package_py.nav_action_server:main',
+            'nav_client = my_package_py.nav_action_client:main',
+            'webcam_pub = my_package_py.webcam_pub:main',
+            'arduino_bridge = my_package_py.arduino_bridge:main',
+            'motor_server = my_package_py.motor_server:main',
+            'motor_client = my_package_py.motor_client:main',
+        ],
+    },
+)
